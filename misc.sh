84360d9355f9bdec830d3525b8927014cbf7f30b
Michael Easter <codetojoy@gmail.com>
2022-10-30 10:34:05 -0300

    new misc action

---
+++ misc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e 
+
+which node
+
+if [ $? = 0 ]; then
+    node --version
+else
+    echo "node not found"
+fi
+
+which npm
+
+if [ $? = 0 ]; then
+    npm --version
+else
+    echo "npm not found"
+fi
+
+which python
+
+if [ $? = 0 ]; then
+    python --version
+else
+    echo "python not found"
+fi
+
+
