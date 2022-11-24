5d48bd949a797d7f8942af6f533d2c7c4e98009c
Michael Easter <codetojoy@gmail.com>
2022-11-24 14:09:31 -0400

    sanity check for JS action

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,10 @@
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec);
+
+function run() {
+    core.notice('TRACER from deploy-s3-javascript');
+}
+
+run();
