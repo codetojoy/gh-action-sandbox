c77db117157d07253d6f378d66ba598295b90f51
Michael Easter <codetojoy@gmail.com>
2022-11-29 17:36:53 -0400

    input/output for p9

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,7 +4,13 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    core.notice('TRACER from deploy-s3-javascript');
+    core.notice('TRACER deploy-s3-javascript begin');
+
+    const foo = core.getInput('foo', {required: true});
+    const bar = `${foo} :: ${Date.now()}`;
+    core.setOutput('bar', bar); 
+
+    core.notice('TRACER deploy-s3-javascript end');
 }
 
 run();
